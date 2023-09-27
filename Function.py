4be511ae8815417cdb222a8b5afd6646d0f5c667
John Payne <W0474679campus.nscc.ca>
2023-09-27 09:01:15 -0300

    Function question printing all numbers on one line

---
+++ Function.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(1, n+1):
+        print(i, end="")
+
+#within the for loop statement I am telling the code to get a range of numbers that start at 1
+#and ends at the n input plus 1. Although the n+1 is not to be included within the code. 
+
+#In the print statement I am telling the program to print the iterations of i and I use the end="" parameter
+#to make sure that all of the numbers are on the same line when outputted. 
